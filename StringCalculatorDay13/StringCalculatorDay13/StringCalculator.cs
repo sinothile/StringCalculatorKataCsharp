c6d45a9b78016b72abe96f9cbee9dd36fc5d548b
sinothilem <sinothilem@D987321>
2018-08-01 08:28:36 +0200

    done

---
+++ StringCalculatorDay13/StringCalculatorDay13/StringCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorDay13
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (NullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+            var numberStorage = SplitNumbers(numbers);
+            var negativeNumbers = numberStorage.Where(x => int.Parse(x) < 0);
+            if (NumbersAre(negativeNumbers))
+            {
+                throw new Exception($"Negatives Not Allowed {JoinNegativeNumbers(negativeNumbers)}");
+            }
+            var sum = GetSum(numberStorage);
+            return sum;
+        }
+
+        private int GetSum(string[] numberStorage)
+        {
+            return numberStorage.Where( x => int.Parse(x) <= 1000).Sum(x => int.Parse(x));
+        }
+
+        private string[] SplitNumbers(string numbers)
+        {
+            return numbers.Split(new char[] { ',', '\n', ';', '/','*','[',']','%'}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private string JoinNegativeNumbers(IEnumerable<string> negativeNumbers)
+        {
+            return string.Join(" ", negativeNumbers);
+        }
+
+        private bool NumbersAre(IEnumerable<string> negativeNumbers)
+        {
+            return negativeNumbers.Any();
+        }
+
+        private bool NullOrWhiteSpace(string number)
+        {
+            return string.IsNullOrWhiteSpace(number);
+        }
+    }
+}
---
+++ StringCalculatorDay13/StringCalculatorDay13Tests/StringCalculatorTests.cs
@@ -0,0 +1,143 @@
+﻿using NUnit.Framework;
+using StringCalculatorDay13;
+using System;
+
+namespace StringCalculatorDay13Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Add_GivenAStringWithEmptyOrNullOrWhiteSpace_ShouldReturn0(string number)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(number);
+
+            //Assert
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("5",5)]
+        public void Add_GivenASingleNumber_ShouldReturnThatNumber(string number, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(number);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("5,5", 10)]
+        [TestCase("10,5,6", 21)]
+        public void Add_GivenMultipleNumbersSeparatedByComma_ShouldReturnTheirSum(string numbers, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1\n2,3", 6)]
+        [TestCase("10\n20,30", 60)]
+        [TestCase("100\n100,3", 203)]
+        public void Add_GivenMultipleNumbersSeparatedByCommaAndNewLineInBetween_ShouldReturnTheirSum(string numbers, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//;\n10;20;50;10;10", 100)]
+        public void Add_GivenMultipleNumbersSeparatedByDifferentDelimeters_ShouldReturnTheirSum(string numbers, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//;\n-10;20", "Negatives Not Allowed -10")]
+        [TestCase("1\n-2,-3", "Negatives Not Allowed -2 -3")]
+        [TestCase("10,-5,6", "Negatives Not Allowed -5")]
+        public void Add_GivenNegativeNumbers_ShouldReturnNegativesNotAllowedAndThoseNegativeNumbers(string numbers, string expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = Assert.Throws<Exception>(() => sut.Add(numbers));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [TestCase("//;\n1000;3;3", 1006)]
+        [TestCase("//;\n999;3;3", 1005)]
+        [TestCase("//;\n1001;3;3", 6)]
+        public void Add_GivenNumbersGreaterThan1000_ShouldIgnoreThem(string numbers, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//[***]\n1***2***3",6)]
+        [TestCase("//[*********]\n1**********2**********3",6)]
+        [TestCase("//[**************]\n10***********20*************30***********40************100",200)]
+        public void Add_GivenMultipleNumbersSeparatedByDelimeterInBrackets_ShouldReturnTheirSum(string numbers, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("//[*][%]\n1*2%3",6)]
+        [TestCase("//[*****][%%%%]\n10*****20%%%%%30",60)]
+        [TestCase("//[***%%**%%][%%%**%%**]\n1***%%**%%2%%%**%%**3**%%**%%5**%%10",21)]
+        public void Add_GivenMultipleNumbersSeparatedByDifferentDelimetersInBrackets_ShouldReturnTheirSum(string numbers, int expected)
+        {
+            //Arrange
+            var sut = new StringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
